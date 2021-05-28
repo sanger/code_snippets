cfac5e274825ab85d0ef10d0b63537f2180b49fb
James Glover <james.glover@sanger.ac.uk>
2021-05-28 08:47:36 +0100

    Add well helpers

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # code_snippets
+
 A repository for useful code-snippets which are a bit too small for a full gem/package
+
+## Adding a new snippet
+
+```bash
+cp LANGUAGE/_template.* SNIPPET_NAME
+```
---
+++ ruby/_template.rb
@@ -0,0 +1,14 @@
+#
+# = Snippet Name
+#
+# A short description of what your snippet does
+#
+# = Example
+#
+# ```ruby
+# shout_it_backwards('hello') # => "OLLEH"
+# ```
+#
+# = Requirements and assumptions
+#
+# [Optional]
